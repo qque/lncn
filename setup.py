4a364042ddef1536abd03a8b235b632cb4cf8f80
qque <qwertystr@protonmail.com>
2025-10-31 00:38:57 -0700

    lncn

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="lncn",
     version="0.1.0",
-    description="simple python cli to count SLOC in a project",
+    description="Simple Python CLI to count SLOC in a codebase",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="qque",
